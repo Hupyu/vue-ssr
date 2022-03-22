ecd9811c19e99ac04f7939e233e3b810e0f9b322
hupuyu <15707972778@163.com>
2022-03-23 00:34:06 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+    <div id="nav">
+      <router-link to="/user">user</router-link> |
+      <router-link to="/setting">setting</router-link>
+    </div>
   <router-view></router-view>
 </template>
 
@@ -7,3 +11,13 @@ export default {
   name: 'App',
 }
 </script>
+<style>
+#app{
+  margin: 0 auto;
+  width: 700px;
+}
+#nav{
+  margin-bottom: 20px;
+  text-align: center;
+}
+</style>
